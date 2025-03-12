09284738d62de2c5190b9ee57d230dc236c6113a
André Oliveira <andre.d.oliveira@tecnico.ulisboa.pt>
2025-03-12 16:07:18 +0000

    Motor activates if object is detected

---
+++ motor/motor.ino
@@ -0,0 +1,51 @@
+//Constantes Sensor
+const int trigPin = 12;
+const int echoPin = 13;
+
+float duration, distance;
+float max_distance = 20;
+//Constantes Motor
+const int controlPin1 = 2;
+const int controlPin2 = 3;
+const int enablePin = 9;
+int motorSpeed = 100;
+
+void setup() {
+  pinMode(controlPin1, OUTPUT);
+  pinMode(controlPin2, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, LOW);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite(controlPin1, HIGH);
+  digitalWrite(controlPin2, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  distance = detectObject();
+  if (distance <= max_distance){
+    startMotor();
+  }
+  delay(5000);
+}
+
+float detectObject(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  return distance;
+}
+
+void startMotor(){
+  analogWrite(enablePin, motorSpeed);
+  delay(2000);
+  analogWrite(enablePin, 0);
+}
